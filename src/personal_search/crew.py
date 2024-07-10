93cbf9745f4b604035a245f6522e4cd00cd38f5a
Cristian C Santos <perestra.ccds@gmail.com>
2024-07-09 23:16:46 -0300

    🚧Ajuste prompts e separação de agentes

--- src/personal_search/crew.py
+++ src/personal_search/crew.py
@@ -1,22 +1,8 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
-from personal_search.tools.books import LibraryTool, YoutubeTool
+from personal_search.tools.books import LibraryTool
 from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=0)
-
-# Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
-
-manager = Agent(
-    role="Project Manager",
-    goal="Efficiently manage the crew and ensure high-quality task completion",
-    backstory="""You're an experienced project manager, skilled in overseeing complex projects and 
-      guiding teams to success. Your role is to coordinate the efforts of the crew members, 
-      ensuring that each task is completed on time and to the highest standard.""",
-	verbose=True,
-	llm=llm,
-	allow_delegation=True,
-)
 
 @CrewBase
 class PersonalSearchCrew():
@@ -31,21 +17,9 @@ class PersonalSearchCrew():
 			tools=[LibraryTool()],
 			verbose=True,
 			llm=llm,
-    		allow_delegation=False,
-			async_mode=True
+    		allow_delegation=False
 		)
 	
-	@agent
-	def youtube_researcher(self) -> Agent:
-		return Agent(
-			config=self.agents_config['youtube_researcher'],
-			tools=[YoutubeTool()],
-			verbose=True,
-			llm=llm,
-			allow_delegation=False,
-			async_mode=True
-		)
-
 	@agent
 	def final_revisor(self) -> Agent:
 		return Agent(
@@ -61,13 +35,6 @@ class PersonalSearchCrew():
 			config=self.tasks_config['research'],
 			agent=self.researcher(),
 		)
-	
-	@task
-	def youtube_research(self) -> Task:
-		return Task(
-			config=self.tasks_config['youtube_research'],
-			agent=self.youtube_researcher(),
-		)
 
 	@task
 	def final_revision(self) -> Task:
@@ -82,9 +49,9 @@ class PersonalSearchCrew():
 		return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
-			#process=Process.sequential,
+			process=Process.sequential,
 			verbose=2,
 			llm=llm,
-			manager_agent=manager,
-			process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+			#manager_agent=manager,
+			#process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
 		)
